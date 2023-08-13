a3db5e5ee7e04567cbd34b0bbfe8399d3759120a
Tadej Supukovic <tadej.supukovic@gmail.com>
2023-08-13 20:41:55 +0200

    Remove experimental scripts from main

--- Various/tdspk - Playground.lua
+++
@@ -1,3 +0,0 @@
-rv, track_id, fx, param_id = reaper.GetLastTouchedFX()
-track = reaper.GetTrack(0, track_id - 1) --get track of last touched FX param
-rv, a = reaper.TrackFX_GetNamedConfigParm(track, fx, "param.1.acs.active")
