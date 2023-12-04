464d8f56d653625a16201c1cf3720f52818b52eb
Tadej Supukovic <tadej.supukovic@gmail.com>
2023-12-04 19:44:26 +0100

    Update tdspk - Memes - ReaDad.lua
    Renamed it to ReaDad

--- Memes/tdspk - Memes - ReaDad.lua
+++ Memes/tdspk - Memes - ReaDad.lua
@@ -3,5 +3,5 @@ response = reaper.ExecProcess("curl -s https://icanhazdadjoke.com/", 0)
 response = string.sub(response, 2)
 
 if response ~= nil then
-  reaper.ShowMessageBox(response, "Dad Joke", 0)
+  reaper.ShowMessageBox(response, "ReaDad", 0)
 end
