65579560a58facffacb2f7596992d0112c1d22b3
Tadej Supukovic <tadej.supukovic@gmail.com>
2023-12-12 17:34:32 +0100

    Create tdspk - Editing - Media Item Spacer.lua

---
+++ Editing/tdspk - Editing - Media Item Spacer.lua
@@ -0,0 +1,85 @@
+-- @description Media Item Spacer
+-- @version 1.0.0
+-- @author Tadej Supukovic (tdspk)
+-- @changelog
+--   First version
+
+dofile(reaper.GetResourcePath() .. '/Scripts/ReaTeam Extensions/API/imgui.lua')('0.8')
+
+local ctx = reaper.ImGui_CreateContext('Media Item Spacer')
+
+min_spacing = 1
+max_spacing = 10
+spacing = 5
+
+local function CacheSelectedTracks()
+  tracks = {} 
+   track_count = reaper.CountSelectedTracks(0)
+   
+   -- cache selected tracks for later selection & deselection
+   for i = 0, track_count - 1 do
+     track = reaper.GetSelectedTrack(0, i)
+     table.insert(tracks, track)
+   end
+   
+   return tracks
+end
+
+local function RenderWindow()
+  track_count = reaper.CountSelectedTracks(0)
+  
+  if track_count > 0 then
+    reaper.ImGui_Text(ctx, track_count .. " tracks selected.")
+    
+    rv, spacing = reaper.ImGui_SliderInt(ctx, "Spacing", spacing, min_spacing, max_spacing, "%d seconds")
+    if rv then
+      tracks = CacheSelectedTracks()
+      
+      reaper.Undo_BeginBlock()
+      
+      for k, track in pairs(tracks) do
+        reaper.SetTrackSelected(track, true)
+        reaper.Main_OnCommand(40421, 0) -- Item: Select all items in track
+        
+        count = reaper.CountTrackMediaItems(track)
+        
+        for i = 0, count - 1 do
+          item = reaper.GetSelectedMediaItem(0, i)
+          
+          if i == 0 then
+            current_pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+          end
+          
+          reaper.SetMediaItemPosition(item, current_pos, false)
+          current_pos = current_pos + spacing
+        end
+        
+        reaper.SetTrackSelected(track, false)
+      end
+      
+      for k, track in pairs(tracks) do
+        reaper.SetTrackSelected(track, true)
+      end
+      
+      reaper.UpdateArrange()
+      
+      reaper.Undo_EndBlock("Media Item Spacer - spaced items for " .. spacing .. " seconds.", 0)
+    end
+  else
+    reaper.ImGui_Text(ctx, "Please select a track")
+  end
+end
+
+local function Loop()
+  reaper.ImGui_SetNextWindowSize(ctx, 300, 100)
+  local visible, open = reaper.ImGui_Begin(ctx, 'Media Item Spacer', true)
+  if visible then
+    RenderWindow()
+    reaper.ImGui_End(ctx)
+  end
+  if open then
+    reaper.defer(Loop)
+  end
+end
+
+Loop()
